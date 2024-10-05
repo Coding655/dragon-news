cd9e10066d1f0120221d014c3267c517c67531ad
Imran Sirajee <153982099+Coding655@users.noreply.github.com>
2024-10-06 00:39:52 +0600

    Update firebase.init.js

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -1,17 +1,1 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBqYx8_8e7a80FmCCFseyb3f9E4m7wJRqQ",
-  authDomain: "dragon-news-f4b98.firebaseapp.com",
-  projectId: "dragon-news-f4b98",
-  storageBucket: "dragon-news-f4b98.appspot.com",
-  messagingSenderId: "240478450077",
-  appId: "1:240478450077:web:31e2a35fcd17e7e5413f74"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
